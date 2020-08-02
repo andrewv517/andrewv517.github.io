6a5f8a448c1afe631984ae74dfc43ecd94428a1c
Andrew <andrewvad517@gmail.com>
2020-08-02 18:49:39 -0400

    fixed hamburger

--- js/main.js
+++ js/main.js
@@ -56,3 +56,8 @@ function removeThis() {
     }
   }, 50)
 }
+
+function clicked() {
+  document.querySelector('.nav-links').classList.toggle('nav-active')
+  document.querySelector('.burger').classList.toggle('toggle')
+}
